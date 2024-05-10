70f6ccf0a8fc42ef595db9f575b1bee30f0e11cf
Aya <47668950+AyaPK@users.noreply.github.com>
2024-05-10 18:51:04 +0100

    Add basic pokemon searching

--- pokedex/src/Pokedex.js
+++ pokedex/src/Pokedex.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useState } from "react";
+import { PokemonType } from "./PokemonType";
 
 export function Pokedex() {
+  const pokemonToShow = ["Bulbasaur", "Squirtle", "Charmander", "Pikachu", "Butterfree", "Articuno", "Sigilyph"]
+  const [selectedPokemon, setSelectedPokemon] = useState(undefined);
+
+  async function getPokemonInfo(name) {
+    const URL = `https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`;
+
+    const response = await fetch(URL);
+    const data = await response.json();
+
+    setSelectedPokemon(data);
+  }
+
   return (
     <div>
-      <h4>
-        Edit <code>src/Pokedex.js</code> and save to reload.
-      </h4>
+      {selectedPokemon && (
+        <div>
+          {selectedPokemon.species.name}
+
+          <PokemonType type={selectedPokemon.types[0].type.name} />
+        </div>
+      )}
+
+      <p>Select Pokemon:</p>
+
+      {
+        pokemonToShow.map(p => (
+          <button onClick={() => getPokemonInfo(p)}>{p}</button>
+        ))
+      }
+
+      
     </div>
   );
 }
