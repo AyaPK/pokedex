03674fbf2bcd64b0b0f82548a34186bec8f12bce
Aya <47668950+AyaPK@users.noreply.github.com>
2024-05-10 18:06:58 +0100

    Add react and update gitignore

---
+++ pokedex/src/Pokedex.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export function Pokedex() {
+  return (
+    <div>
+      <h4>
+        Edit <code>src/Pokedex.js</code> and save to reload.
+      </h4>
+    </div>
+  );
+}
