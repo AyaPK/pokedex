a40114107ba7e0365e64b15b6d687fc0a638e7e1
AyaPK <Sherbnya@gmail.com>
2024-05-11 19:54:20 +0100

    Allow changing sprite

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -4,27 +4,34 @@ import "./styles.css";
 
 export default function App() {
   const [shownPokemon, setShownPokemon] = useState(undefined);
+  const [currentOffset, setCurrentOffset] = useState(0);
+  
+
+  async function getInfo(offset) {
+    const URL = "https://pokeapi.co/api/v2/pokemon/?offset="+(offset*20);
+    setCurrentOffset(offset)
+
+    try {
+      const response = await fetch(URL);
+      const data = await response.json();
+      setShownPokemon(data);
+    } catch (error) {
+      console.error("Error fetching Pokemon data:", error);
+    }
+  }
 
   useEffect(() => {
-    async function getInfo() {
-      const URL = "https://pokeapi.co/api/v2/pokemon/?limit=151";
-
-      try {
-        const response = await fetch(URL);
-        const data = await response.json();
-        setShownPokemon(data);
-      } catch (error) {
-        console.error("Error fetching Pokemon data:", error);
-      }
-    }
-
-    getInfo();
+    getInfo(currentOffset);
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
           <h2>Aya's Pokédex</h2>
+          {(currentOffset > 0 && 
+          <button onClick={() => getInfo(currentOffset-1)}>&lt;=</button>
+            )}
+          <button onClick={() => getInfo(currentOffset+1)}>=&gt;</button>
       </header>
 
       <body>
--- pokedex/src/Pokedex.js
+++ pokedex/src/Pokedex.js
@@ -3,15 +3,18 @@ import { PokemonType } from "./PokemonType";
 
 export function Pokedex(props) {
   const [selectedPokemon, setSelectedPokemon] = useState(null);
+  const [shownSprite, setShownSprite] = useState(null);
+  const [shinyToggle, setShinyToggle] = useState(false);
+  const [chosenGender, setChosenGender] = useState("default");
+  const [facingDirection, setFacingDirection] = useState("front");
 
   useEffect(() => {
     async function fetchData() {
       try {
-        console.log(props.name)
-
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${props.name}`);
         const data = await response.json();
         setSelectedPokemon(data);
+        setShownSprite(data.sprites.front_default);
       } catch (error) {
         console.error("Error fetching Pokemon data:", error);
       }
@@ -19,6 +22,41 @@ export function Pokedex(props) {
 
     fetchData();
   }, [props.name]);
+
+  async function getSprite(direction, gender) {
+    setFacingDirection(direction);
+    setChosenGender(gender);
+
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${props.name}`);
+    const data = await response.json();
+    if(gender === "default" && shinyToggle === false) {
+      setShownSprite(data.sprites[direction+"_default"])
+    } else if(gender === "default" && shinyToggle === true) {
+      setShownSprite(data.sprites[direction+"_shiny"])
+    } else if(gender === "female" && shinyToggle === false) {
+      setShownSprite(data.sprites[direction+"_female"])
+    } else if(gender === "female" && shinyToggle === true) {
+      setShownSprite(data.sprites[direction+"_shiny_female"])
+    }
+  }
+
+  function toggleShiny() {
+    setShinyToggle(prevShinyToggle => !prevShinyToggle);
+  }
+  useEffect(() => {
+    getSprite(facingDirection, chosenGender)
+  }, [shinyToggle]);
+
+  function rotate() {
+    if(facingDirection === "front") {
+      setFacingDirection("back")
+    } else {
+      setFacingDirection("front")
+    }
+  }
+  useEffect(() => {
+    getSprite(facingDirection, chosenGender)
+  }, [facingDirection]);
 
   return (
     <div>
@@ -29,9 +67,22 @@ export function Pokedex(props) {
           {selectedPokemon.types.map((type) => (
             <PokemonType key={type.type.name} type={type.type.name} />
           ))}
-          <img src={selectedPokemon.sprites.front_default} alt={selectedPokemon.species.name} />
+          <img src={shownSprite} alt={selectedPokemon.species.name} />
+          <hr/>
+          <button className={shinyToggle ? "shiny-enabled" : "shiny-disabled"} onClick={() => toggleShiny()}>✨toggle Shiny</button>
+          <br/>
+
+          <button onClick={() => rotate()}>rotate</button>
+          {(selectedPokemon.sprites.front_female && 
+          <div>
+            <button onClick={() => getSprite(facingDirection, "default")}>♂</button>
+            <button onClick={() => getSprite(facingDirection, "female")}>♀</button>
+          </div>
+            )}
         </div>
       )}
+      
+      
     </div>
   );
 }
