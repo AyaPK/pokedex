5cba47c2dc3995d3dba5e2f723ec8d27340c9a93
AyaPK <Sherbnya@gmail.com>
2024-05-13 15:01:58 +0100

    create helpermethods

--- src/PokemonVarieties.js
+++ src/PokemonVarieties.js
@@ -1,19 +1,7 @@
 import React from "react";
+import { getVarietyName } from "./helperMethods";
 
 export function PokemonVariety(props) {
-    function getVarietyName(input) {
-        if (input === props.name) {
-            return "default";
-        } else {
-            const baseIndex = input.indexOf(props.name);
-            if (baseIndex !== -1) {
-                return input.slice(baseIndex+1 + props.name.length);
-            } else {
-                return input;
-            }
-        }
-    }
-
     function getNamesFromVarietes() {
         const vars = props.varieties.map(p => (
             p.pokemon.name
@@ -30,7 +18,7 @@ export function PokemonVariety(props) {
             <select className="variety-dropdown" onChange={handleVarietyChange}>
                 {getNamesFromVarietes().map((option, index) => (
                     <option key={index} value={option}>
-                        {getVarietyName(option)}
+                        {getVarietyName(option, props.name)}
                     </option>
                 ))}
             </select>
